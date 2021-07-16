77f1180305914d35e05e27a8ff2a61a082517ac5
vcokltfre <vcokltfre@gmail.com>
2021-07-16 07:11:00 +0100

    chore: add static file serving

--- src/api/main.py
+++ src/api/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import router
 
 
 app = FastAPI(openapi_url=None)
+
+app.mount("/static", StaticFiles(directory="src/web/static"), "static")
 
 app.include_router(router)
