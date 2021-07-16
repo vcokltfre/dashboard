3967aae49df1705b87cb808e7d9c8b034a0ee202
vcokltfre <vcokltfre@gmail.com>
2021-07-16 07:56:36 +0100

    chore: add db and redis uri to env loader

--- src/api/env.py
+++ src/api/env.py
@@ -10,3 +10,11 @@ class Site:
     name: str = getenv("SITE_NAME", "Dashboard")
     icon: str = getenv("SITE_ICON", "https://avatars.githubusercontent.com/u/16879430?v=4")
     repo: str = getenv("SITE_REPO", "https://github.com/vcokltfre/dashboard")
+
+
+class Database:
+    uri: str = getenv("DATABASE_URI", "postgres://dashboard:dashboard@localhost:5432/dashboard")
+
+
+class Redis:
+    uri: str = getenv("REDIS_URI", "redis://localhost:6379")
