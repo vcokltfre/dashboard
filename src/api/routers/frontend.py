f98f37957df46cd69b7bef81faf288020c1ea05a
vcokltfre <vcokltfre@gmail.com>
2021-07-16 08:43:28 +0100

    feat: add guilds page

--- src/api/routers/frontend.py
+++ src/api/routers/frontend.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
 from src.api.env import Site
+from src.api.objects.guild import Guild
 
 
 router = APIRouter()
@@ -14,4 +15,12 @@ async def get_index(request: Request) -> HTMLResponse:
     return templates.TemplateResponse("index.html", {
         "request": request,
         "site": Site,
+    })
+
+@router.get("/guilds")
+async def get_guilds(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse("guilds.html", {
+        "request": request,
+        "site": Site,
+        "guilds": []
     })
